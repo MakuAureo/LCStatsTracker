0016245503cfa777af840330c9abe7397caf3938
MakuAu <makuaureo@gmail.com>
2026-06-02 10:07:53 -0300

    Fixed possible null exception when other players disconnect

--- Patches/PlayerTracker.cs
+++ Patches/PlayerTracker.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using GameNetcodeStuff;
 using HarmonyLib;
 using Unity.Netcode;
@@ -6,10 +7,29 @@ namespace StatsTracker.Patches;
 
 internal class PlayerTracker
 {
+  private static readonly Dictionary<int, ulong> allConnectedPlayersIDToSteamIDs = [];
+
   public static void ApplyPlayerTrackerPatches(Harmony Harmony)
   {
+    Harmony.Patch(AccessTools.Method(typeof(StartOfRound), nameof(StartOfRound.ResetPlayersLoadedValueClientRpc)), prefix: new HarmonyMethod(typeof(PlayerTracker), nameof(RegisterAllConnectedPlayers)));
     Harmony.Patch(AccessTools.Method(typeof(PlayerControllerB), nameof(PlayerControllerB.KillPlayerClientRpc)), prefix: new HarmonyMethod(typeof(PlayerTracker), nameof(TrackDeath)));
     Harmony.Patch(AccessTools.Method(typeof(StartOfRound), nameof(StartOfRound.OnPlayerDC)), prefix: new HarmonyMethod(typeof(PlayerTracker), nameof(TrackDisconnect)));
+  }
+
+  private static void RegisterAllConnectedPlayers(StartOfRound __instance)
+  {
+    if ((GameNetworkManager.Instance.gameVersionNum > 72 && __instance.__rpc_exec_stage != NetworkBehaviour.__RpcExecStage.Execute) || (GameNetworkManager.Instance.gameVersionNum <= 72 && __instance.__rpc_exec_stage != NetworkBehaviour.__RpcExecStage.Client))
+      return;
+
+    allConnectedPlayersIDToSteamIDs.Clear();
+
+    for (int i = 0; i < __instance.allPlayerScripts.Length; i++)
+    {
+      PlayerControllerB? playerScript = __instance.allPlayerScripts[i];
+      if (playerScript == null) continue;
+
+      allConnectedPlayersIDToSteamIDs.Add(i, playerScript.playerSteamId);
+    }
   }
 
   private static void TrackDeath(PlayerControllerB __instance, int causeOfDeath)
@@ -24,6 +44,8 @@ internal class PlayerTracker
 
   private static void TrackDisconnect(StartOfRound __instance, int playerObjectNumber)
   {
-    StatsTracker.DayStats!.Players[__instance.allPlayerScripts[playerObjectNumber].playerSteamId].Disconnect();
+    if (!StatsTracker.dayHasStarted) return;
+
+    StatsTracker.DayStats!.Players[allConnectedPlayersIDToSteamIDs[playerObjectNumber]].Disconnect();
   }
 }
